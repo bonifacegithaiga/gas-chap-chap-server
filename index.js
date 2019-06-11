206085e6189869b86dffb6b590802fa458392690
unknown <bonifacegithaiga95@gmail.com>
2019-06-11 17:02:33 +0300

    installed aded.environment

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const mysql =require("mysql");
 const app = express ();
 const pool = mysql.createPool({
-     host:"localhost",
-     user: "root",
-     password:"password",
-     database: "gas_chap_chap"                     
+     host:process.env.DB_HOST,
+     user:process.env.DB_USER,
+     password:process.env.DB_PASSWORD,
+     database:  process.env.DB_DATABASE                  
 })
-const name = "boniface githaiga";
+// const name = "boniface githaiga";
 
-app.get("/", (req, res ) => res.send("hello "+ name));
+// app.get("/", (req, res ) => res.send("hello "+ name));
 
 app.get("/api/gas", (req, res) => {
-    pool.query("SELECT id, name FROM gas", (error, rows) =>{
+    pool.query("SELECT * FROM gas", (error, rows) =>{
         if (error) {
             return res.status(500).json({error});
         }
         res.json(rows);
+        console.log(rows)
     });  
 });
-app.listen(9000, () => console.log("App listening on port 9000"));
+app.listen(9000, () => {console.log("App listening on port 9000")});
