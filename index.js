b61e35ff6ab706cd6c02aa9abbf8ae79281dcdaf
unknown <bonifacegithaiga95@gmail.com>
2019-06-11 12:20:38 +0300

    add/api/gas root

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql =require("mysql");
 const app = express ();
+const pool = mysql.createPool({
+     host:"localhost",
+     user: "root",
+     password:"password",
+     database: "gas_chap_chap"                     
+})
 const name = "boniface githaiga";
 
 app.get("/", (req, res ) => res.send("hello "+ name));
+
+app.get("/api/gas", (req, res) => {
+    pool.query("SELECT id, name FROM gas", (error, rows) =>{
+        if (error) {
+            return res.status(500).json({error});
+        }
+        res.json(rows);
+    });  
+});
 app.listen(9000, () => console.log("App listening on port 9000"));
