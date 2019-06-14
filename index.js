9bed74653ab392b3181321b26b3067ded4c8b621
unknown <bonifacegithaiga95@gmail.com>
2019-06-14 17:47:32 +0300

    created end points

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const app = express ();
 const pool = mysql.createPool({
      host:process.env.DB_HOST,
      user:process.env.DB_USER,
-     password:process.env.DB_PASSWORD,
+     password:process.env.DB_PASSWORD,       
      database:  process.env.DB_DATABASE                  
 })
 // const name = "boniface githaiga";
 
 // app.get("/", (req, res ) => res.send("hello "+ name));
 
-app.get("/api/gas", (req, res) => {
-    pool.query("SELECT * FROM gas", (error, rows) =>{
+app.get("/api/home", (req, res) => {
+    pool.query("select image_url,type_of_gas,price FROM gas", (error, rows) =>{
         if (error) {
             return res.status(500).json({error});
         }
@@ -21,4 +21,36 @@ app.get("/api/gas", (req, res) => {
         console.log(rows)
     });  
 });
-app.listen(9000, () => {console.log("App listening on port 9000")});
+app.get("/api/customer", (req, res)=>{
+    pool.query("insert into clients (location,username,phone_no,gas_id) values ('kayole','jane',0700500879,1)", (error,rows) =>{
+        if (error) {
+            return res.status(500).json({error});
+        }
+        res.json(rows)
+    });
+
+});
+app.get("/api/order", (req,res)=>{
+    pool.query("select g.image_url,g.type_of_gas,g.weight,c.username FROM gas AS g join clients AS c ON g.gas_id=c.gas_id",(error,rows)=>{
+        if(error){
+            return res.status(500).json({error});
+        }
+        else{
+            return res.json(rows)
+        }
+    });
+
+})
+app.get("/api/admin", (req,res)=>{
+    pool.query("",(error,rows)=>{
+        if(error){
+            return res.status(500).json({error});
+        }
+        else{
+            return res.json(rows)
+        }
+    });
+
+})
+
+app.listen(9000, ()=>{console.log("running server")})
