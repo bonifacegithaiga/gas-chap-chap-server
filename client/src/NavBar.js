2a8d1186837caefddbdc346b135095ed6ef9ae24
unknown <bonifacegithaiga95@gmail.com>
2019-07-02 18:59:16 +0300

    added navbaar.js

---
+++ client/src/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NarBar() {
+    return (
+        <div className="gcc-container">
+            <nav className="gcc-nav">
+                 <span className="gcc-title">Gas Chap Chap</span>
+                 <a href="/">Home</a>
+                 <a href="/">Customer</a>
+                 <a href="/">Order</a>
+                 <a href="/">Admin</a>
+                 
+            </nav>
+        </div>
+    );
+}
+    
+export default NavBar;
