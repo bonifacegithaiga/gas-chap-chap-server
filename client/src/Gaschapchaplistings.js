581d767e46ccdd79d09caacff7afa5ec1c69b7f3
unknown <bonifacegithaiga95@gmail.com>
2019-07-30 07:32:02 +0300

    added js.display

--- client/src/Gaschapchaplistings.js
+++ client/src/Gaschapchaplistings.js
@@ -1,19 +1,57 @@
 import React from "react";
+import axios from "axios";
+import GasChapChapDisplay from "./GasChapChapDisplay";
 
-import Gaschapchap from "./Gaschapchap";
-function Gaschapchaplistings({gaschapchapdisplay}){
+
+  class Gaschapchaplistings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           gaschapchapDispaly: [],
+        };
+    }
+    
+    componentDidMount() {
+        this.fetchgaschapchapdisplay();
+    }
+
+    fetchgaschapchapdisplay(){
+        this.setState({ loading: true, error: false });
+
+        axios
+        .get("http://localhost:9000/api/gaschapchapdisplay")
+        .then(Response => {
+            this.setState({
+                gaschapchapDisplay:Response.data,
+                loading: false,
+                error: false
+            });
+        })
+        .catch(error => {
+            this.setState({
+                gaschapchapDisplay: [],
+                loadind:false,
+                error: true
+            });
+        });
+    }
+    render() {
+        const { gaschapchapdisplay } = this.setState;
+
     return(
         <div className="gcc-container">
         <div className="gcc-gaschapchap-listings">
         
-        {
-            gaschapchapdisplay.map(m=> (
-                <Gaschapchap key={m.id} gaschapchapdisplay={m} />
-            ))
-        }
+        
+           { gaschapchapdisplay.map(m=> (
+                <GashapchapDisplay key={m.id} gaschapchapdisplay={m} />
+            ))}
+        
         </div>
      </div>
     );
+}
+
 }
 
 export default Gaschapchaplistings;
