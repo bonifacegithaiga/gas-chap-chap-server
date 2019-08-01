dc9ad57bc4cd2c2d0ed9af9eb512fca68da2c940
unknown <bonifacegithaiga95@gmail.com>
2019-08-01 18:24:03 +0300

    added orderpage.js customerpage.js adminpage.js

---
+++ client/src/AdminPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminPage() {
+    return (
+        <div className="admin">
+            <h1>ADMIN-PAGE</h1>
+            
+        </div>
+    );
+}
+
+export default AdminPage;
